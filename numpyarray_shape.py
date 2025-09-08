3e020da13786935635bb86f0711d1b867941a41c
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-09-08 18:42:27 -0500

    Create numpyarray_shape.py

---
+++ numpyarray_shape.py
@@ -0,0 +1,8 @@
+#shape of an array is the no.of elements in each dimension
+#shape of an array returns a tuple with the values of no.of elements n each dimension
+import numpy as np
+arr= np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+print(arr.shape)
+'''
+above code returns (2,4). which means. the above array has 2 elements in one dimension and 4 elements in other dimension
+'''
