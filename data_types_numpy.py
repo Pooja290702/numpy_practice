f077bf648636f89eb7fc67d70a93faa4fa109b13
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-09-07 23:28:26 -0500

    data_types_numpy.py

---
+++ data_types_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array(['apple', 'banana', 'cherry'])
+arr3 = np.array([1, 2, 3, 4], dtype='S')
+arr4 = np.array([1, 2, 3, 4], dtype='i4')
+
+print(arr1.dtype)
+print(arr2.dtype)
+print(arr3.dtype)
+print(arr4.dtype)
