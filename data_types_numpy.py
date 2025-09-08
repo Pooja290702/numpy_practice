cddf80214ee7beb35d474dc6ef455a6b91aef023
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-09-07 23:38:24 -0500

    data_types_numpy.py

--- data_types_numpy.py
+++ data_types_numpy.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 arr1 = np.array([1, 2, 3, 4])
-arr2 = np.array(['apple', 'banana', 'cherry'])
-arr3 = np.array([1, 2, 3, 4], dtype='S')
+arr2 = np.array(['apple', 'banana', 'cherry'])   # this data type is <u6 because, u means unicode STRING TYPE and 6 is the maximum length of the string
+arr3 = np.array([1, 2, 3, 4], dtype='S')          # s means string type but only ascii , not fully unicode. if it says s1, then only 1 char. if we print it, we get b'' bcz it indicates ascii.
 arr4 = np.array([1, 2, 3, 4], dtype='i4')
 
 print(arr1.dtype)
