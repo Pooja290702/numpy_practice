d1cf4eda572c487947b96df69fe01ef5f1bfd3c3
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-09-08 01:11:40 -0500

    Update type_conversion_in_numpy.py

--- type_conversion_in_numpy.py
+++ type_conversion_in_numpy.py
@@ -1,5 +1,10 @@
+
 import numpy as np
+#type1 converting float into int
 arr=np.array([1.2,2.3,3.4,4.5,5.6])
 newarr=arr.astype('i')
 print(newarr.dtype)
-#converting float into int
+#type2
+a2=arr.astype(int)
+a3=arr.astype(bool)
+print(a2,a3)
