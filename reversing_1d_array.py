82f6c2c32ab8c821acb41cd5b310584c3ee3dbce
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-09-12 23:50:48 -0500

    reversing_1d_array.py
    Reversing an array using np.flip and slicing.

--- reversing_1d_array.py
+++ reversing_1d_array.py
@@ -1,4 +1,8 @@
 import numpy as np
 arr= np.array([1,2,3,4,5])
+a1=np.array([6,7,8,9,0])
 print(arr[::-1])
 #arrays cannot be reversed, so we use slicing to reverse an array
+#we can also use np.flip to reverse an array
+a2=np.flip(a1)
+print(a2)
