cf8a768efd38f204fcdd40aa934a8712804289d7
Poojitha Nagilla <103567616+Pooja290702@users.noreply.github.com>
2025-09-15 18:59:27 -0500

    identity prints a n*n array with diagonal elemnts as 1s and other elements as 0s.

---
+++ identity.py
@@ -0,0 +1,4 @@
+#The identity tool returns an identity array. An identity array is a square matrix with all the main diagonal elements as  and the rest as . The default type of elements is float.
+import numpy as np
+row=3
+print(np.identity(3))
